1210c137012bfcd29669d39e08551dd9c52f69cf
Blake Netzeband <blake.netzeband@gmail.com>
2024-07-04 08:26:44 -0600

    update dir structure

--- build.ts
+++ build.ts
@@ -5,32 +5,32 @@ import fs from "fs";
 const buildDir = "build";
 
 (async () => {
-	if (fs.existsSync(buildDir)) {
-		fs.rmSync(buildDir, { recursive: true });
-	}
+  if (fs.existsSync(buildDir)) {
+    fs.rmSync(buildDir, { recursive: true });
+  }
 
-	const res = await build({
-		entryPoints: ["./src/**/index.ts"],
-		outdir: buildDir,
-		bundle: true,
-		minify: true,
-		sourcemap: true,
-		plugins: [
-			copy({
-				resolveFrom: "cwd",
-				assets: [
-					{
-						from: ["./manifest.json"],
-						to: ["./build/manifest.json"],
-					},
-					{
-						from: ["./icons/*.png"],
-						to: ["./build/icons"],
-					},
-				],
-			}),
-		],
-	});
+  const res = await build({
+    entryPoints: ["./src/projects/**/index.ts"],
+    outdir: buildDir,
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    plugins: [
+      copy({
+        resolveFrom: "cwd",
+        assets: [
+          {
+            from: ["./manifest.json"],
+            to: ["./build/manifest.json"],
+          },
+          {
+            from: ["./icons/*.png"],
+            to: ["./build/icons"],
+          },
+        ],
+      }),
+    ],
+  });
 
-	console.log(res);
+  console.log(res);
 })();
