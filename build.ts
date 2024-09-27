cb0b23902ab643cb8c6f97230c73e55bb771736b
Blake Netzeband <blake.netzeband@gmail.com>
2024-09-27 07:47:27 -0600

    sync package.json with manifest version

--- build.ts
+++ build.ts
@@ -1,12 +1,42 @@
 import { build } from "esbuild";
 import { copy } from "esbuild-plugin-copy";
 import fs from "fs";
+import path from "path";
 
 const buildDir = "build";
+
+/**
+ * @param file relative to __dirname
+ */
+function getJsonFromFile(file: string) {
+  const data = fs.readFileSync(path.resolve(__dirname, file), "utf-8");
+
+  return JSON.parse(data);
+}
+
+function syncVersions() {
+  const { version } = getJsonFromFile("package.json");
+  const manifest = getJsonFromFile("manifest.json");
+
+  manifest.version = version;
+
+  fs.writeFileSync(
+    path.resolve(__dirname, "manifest.json"),
+    JSON.stringify(manifest)
+  );
+}
 
 (async () => {
   if (fs.existsSync(buildDir)) {
     fs.rmSync(buildDir, { recursive: true });
+  }
+
+  try {
+    syncVersions();
+  } catch (error) {
+    console.log(
+      "unable to sync `version` value of package.json and manifest.json"
+    );
   }
 
   const res = await build({
